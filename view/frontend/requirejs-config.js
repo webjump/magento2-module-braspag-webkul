b4d2e29c9d665548556fafbdc48847aad2cc287d
Arthur Henrique Wiebusch <arthur.henrique@webjump.com.br>
2022-05-12 09:28:17 -0300

    removing paths from requirejs-config

--- view/frontend/requirejs-config.js
+++ view/frontend/requirejs-config.js
@@ -1,7 +1,4 @@
 var config = {
-    paths: {
-        'Webjump_BraspagPagador/payment/cc-form': 'Braspag_Webkul/payment/cc-form'
-    },
     config: {
         mixins: {
             'Webjump_BraspagPagador/js/view/payment/method-renderer/creditcard': {
