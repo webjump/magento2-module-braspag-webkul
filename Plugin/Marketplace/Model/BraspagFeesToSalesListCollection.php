09ba6eb7a18a48d2998070e5a12ca747212fc06f
Arthur Henrique Wiebusch <arthur.henrique@webjump.com.br>
2022-05-26 16:31:38 -0300

    OBRA-1097: [Cartão com Juros] Detalhes Pedido - Seller

--- Plugin/Marketplace/Model/BraspagFeesToSalesListCollection.php
+++ Plugin/Marketplace/Model/BraspagFeesToSalesListCollection.php
@@ -17,18 +17,9 @@ class BraspagFeesToSalesListCollection
         SalesListCollection $subject,
         $collection
     ) {
-        $collection = $collection->getSelect()
-            ->reset(\Zend_Db_Select::COLUMNS)
+        $collection
             ->columns(
                 [
-                    'main_table.currency_rate AS currency_rate',
-                    'main_table.order_id AS order_id',
-                    'SUM(main_table.total_amount) AS total_amount',
-                    'SUM(main_table.actual_seller_amount) AS total',
-                    'SUM(main_table.actual_seller_amount) AS actual_seller_amount',
-                    'SUM(main_table.total_commission) AS total_commission',
-                    'SUM(main_table.applied_coupon_amount) AS applied_coupon_amount',
-                    'SUM(main_table.total_tax) AS total_tax',
                     'SUM(main_table.braspag_fees) AS braspag_fees',
                     'SUM(main_table.braspag_fees_amount) AS braspag_fees_amount',
                 ]
